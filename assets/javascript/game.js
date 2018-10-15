24221896295b4ae29107a96d0e8057b1aa06741a
Elm <emmal.muniz@gmail.com>
2018-10-14 21:25:57 -0500

    added images tiny bit js

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+
+//start of game//
+
+window.onload = function (){
+    var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    var word = ["rose", "tulip", "lily", "daisy", "sunflower"]
+    //number of lives left//
+    var guessesLeft = 10;
+    //number of spaces in word -- not sure how to identify//
+    var space = ["rose", "tulip", "lily", "daisy", "sunflower"]
+    
+}
