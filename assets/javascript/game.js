ad9312657f0d3599223456e949c92ce07b15675b
Elm <emmal.muniz@gmail.com>
2018-10-18 16:19:19 -0500

    saves for today the 18th

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,74 @@
 
-//start of game//
+//variables//
 
-window.onload = function (){
+
     var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    var word = ["rose", "tulip", "lily", "daisy", "sunflower"]
-    //number of lives left//
-    var guessesLeft = 10;
-    //number of spaces in word -- not sure how to identify//
-    var space = ["rose", "tulip", "lily", "daisy", "sunflower"]
+
+//create array of words//
+    var wordList = [
+        "rose",
+        "lily",
+        "sunflower",
+        "tulip",
+        "daisy",
+        "dahilia",
+        "begonia",
+        "daffodil",
+        "carnation",
+        "irises"
+    ];
+
+    const maxTries = 10;
+
+    //stores letters used//
+    var guessedLetters = [];
+
+    //index of current word in the array//
+    var wordIndex;
     
-}
+    //word we build//
+    var answerWord = [];
+
+    //how many guesses player has left//
+    var remainingGuesses = 0;
+
+    //alert to tell you if game started//
+    var startGame = false;
+
+    //alert "press any key to try again"//
+    var finishedGame = false;
+
+    var wins = 0;
+
+    //restart game//
+    function resetGame() {
+        remainingGuesses = maxTries;
+        startGame = false;
+        wordIndex = Math.floor(Math.random() * (wordList.length));
+        guessedLetters = [];
+        answerWord =[];
+        for (var i = 0; i <wordList[wordIndex]. length; i ++) 
+            answerWord.push ("_");
+        }
+
+        console.log (resetGame)
+    
+
+   
+
+
+
+
+
+    
+
+
+// might need command.....window.onload = function (){
+
+    //if (guessesLeft <= 10 {
+
+        //function alphabetChecker(x) {
+            //var vowels = ['a', 'e', 'i', 'o', 'u']
+            //if (vowels.indexOf(x) !== -1) {
+              //console.log("Found at index:" + //vowels.indexOf(x))//
+            
